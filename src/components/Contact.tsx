848b329e49d3fa89d75edd09d09eec8d89278361
Sayantan G <sayantanghatak54@gmail.com>
2025-07-22 00:27:29 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -294,13 +294,13 @@ const Contact = () => {
                 </span>
               </div>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.0723660642634!2d88.30946361329329!3d22.463914727446266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027ba740e84c69%3A0xe90c52c1d7cc2898!2sOpstean%20Healthcare%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1753123740837!5m2!1sen!2sin" 
-              width="400" 
-              height="300" 
+              width="100%" 
+              height="100%" 
               style="border:0;" 
               allowfullscreen="" 
               loading="lazy" 
-              referrerpolicy="no-referrer-when-downgrade">
-            </iframe>
+              referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </div>
         </div>
