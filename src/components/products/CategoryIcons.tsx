dd04e69e89490f5550ee3ca6c72b9c7f80091898
Sayantan G <sayantanghatak54@gmail.com>
2025-07-21 23:16:51 +0530

    Update CategoryIcons.tsx

--- src/components/products/CategoryIcons.tsx
+++ src/components/products/CategoryIcons.tsx
@@ -23,16 +23,11 @@ export const categoryIcons = {
   "Cardio Care": <Heart className="w-8 h-8" />,
   "Gastro": <Activity className="w-8 h-8" />,
   "General Segment": <Plus className="w-8 h-8" />,
-  "Women Care": <User className="w-8 h-8" />,
-  "Derma Care": <Shield className="w-8 h-8" />,
-  "Cold Management": <Thermometer className="w-8 h-8" />,
-  "Nutritional Supplements": <Leaf className="w-8 h-8" />,
-  "Pain Management": <Zap className="w-8 h-8" />,
   "Urology Care": <Activity className="w-8 h-8" />,
   "Pain Care": <Zap className="w-8 h-8" />,
   "Gyno Care": <Heart className="w-8 h-8" />,
   "Ortho Care": <Shield className="w-8 h-8" />,
   "Cold Care": <Thermometer className="w-8 h-8" />,
-  "Nuro Care": <Brain className="w-8 h-8" />,
+  "Neuro Care": <Brain className="w-8 h-8" />,
   "Derma Care": <Sun className="w-8 h-8" />
 };
